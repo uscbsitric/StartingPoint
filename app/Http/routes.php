b64b2a759f21d32cb846e1634e4bdc67c30eceac
Frederick Sandalo <uscbsitric@gmail.com>
2016-06-17 09:17:02 +0000

    Basic Task List

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
-
+	use App\Task;
+	use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,11 +12,51 @@
 |
 */
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
+Route::get('/', function () 
+				{
+				    return view('welcome');
+				}
+		  );
 
-Route::get('/', 'WelcomeController@index');
-Route::get('contact', 'WelcomeController@contact');
+// Display All Tasks
+Route::get('/allTasks', function()
+						{
+							$tasks = Task::orderBy('created_at', 'asc')->get();
+
+							return view('tasks', compact('tasks'));
+						}
+		 );
+
+Route::post('/tasks', function(Request $request)
+				      {
+					    $validator = Validator::make($request->all(),
+					   								 ['name' => 'required|max:255']
+					   							    );
+					   
+					    if( $validator->fails() )
+					    {
+					       return redirect('/allTasks')->withInput()->withErrors($validator);
+					    }
+				   	   
+					    // Create the task
+					    $task = new Task;
+					    $task->name = $request->name;
+					    $task->save();
+
+					    return redirect('/allTasks');
+				      }
+		   );
+
+// Delete an Existing Task
+Route::get('/task/{id}', function()
+					     {
+						     echo 'deleting an existing task';
+					     }
+		  );
+
+Route::delete('/task/{id}', function ($id)
+							{
+								Task::findOrFail($id)->delete();
+								return redirect('/allTasks');
+							}
+			);
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Laravel Quickstart - Basic</title>
+
+		<link rel="stylesheet" href="/css/all.css">
+		<script type="text/javascript" src="/js/all.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Navbar Contents -->
+			</nav>
+		</div>
+	
+		@yield('content')
+	</body>
+</html>
