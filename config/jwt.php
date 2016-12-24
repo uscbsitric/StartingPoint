9f84b28035403418dec20ff51e2a4df6caacfa40
Frederick Sandalo <uscbsitric@gmail.com>
2016-12-24 07:14:13 +0000

    generated JWT key using php artisan jwt:generate

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'changeme'),
+    'secret' => env('JWT_SECRET', 'Rjch98ydS2fGnAddxJxTlO66w34Czlne'),
 
     /*
     |--------------------------------------------------------------------------
