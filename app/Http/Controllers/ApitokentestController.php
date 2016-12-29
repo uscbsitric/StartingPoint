017ef2b8a505f7378bf30d66c2538a9650678fd2
Frederick Sandalo <uscbsitric@gmail.com>
2016-12-29 01:43:00 +0000

    Added functions register and login in ApitokentestController

--- app/Http/Controllers/ApitokentestController.php
+++ app/Http/Controllers/ApitokentestController.php
@@ -45,4 +45,23 @@ class ApitokentestController extends Controller
 	{
 		return response()->json(['result' => 'This is the result from the get_user_details function of the ApitokentestController']);
 	}
+	
+	public function register(Request $request)
+	{
+		$input = $request->all();
+		$input['password'] = Hash::make($input['password']);
+		User::create($input);
+		return response()->json(['result'=>true]);
+	}
+	
+	public function login(Request $request)
+	{
+		$input = $request->all();
+
+		if (!$token = JWTAuth::attempt($input))
+		{
+			return response()->json(['result' => 'wrong email or password.']);
+		}
+		return response()->json(['result' => $token]);
+	}
 }
