3e6f3974dbdb8b7ff3097647b3273dcfa047dabe
Frederick Sandalo <uscbsitric@gmail.com>
2017-01-09 06:43:27 +0000

    Updated the existing User Model to use the EntrustUserTrait from Entrust

--- app/User.php
+++ app/User.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 use App\Task;
 
@@ -16,7 +17,7 @@ class User extends Model implements AuthenticatableContract,
                                     AuthorizableContract,
                                     CanResetPasswordContract
 {
-	use Authenticatable, Authorizable, CanResetPassword;
+	use Authenticatable, Authorizable, CanResetPassword, EntrustUserTrait;
 
     /**
      * The attributes that are mass assignable.
