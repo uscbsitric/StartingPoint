e2074d4e30f680b6792b4522c6488bed3f2fe240
Frederick Sandalo <uscbsitric@gmail.com>
2017-01-09 05:43:25 +0000

    Installation Step 3: Added Entrust to the aliases array in config/app.php

--- config/app.php
+++ config/app.php
@@ -208,6 +208,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 		'JWTAuth' => 'Tymon\JWTAuth\Facades\JWTAuth',
+    	'Entrust' => Zizaco\Entrust\EntrustFacade::class,
     ],
 
 ];
