916b31db5da8b3a292b914be006bcd72d08a884d
Frederick Sandalo <uscbsitric@gmail.com>
2017-01-19 14:20:44 +0000

    Created EventsTestController for Events Testing

---
+++ app/Http/Controllers/EventsTestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class EventsTestController extends Controller
+{
+    public function eventtest()
+    {
+    	exit('frederick debugging here');
+    }
+}
