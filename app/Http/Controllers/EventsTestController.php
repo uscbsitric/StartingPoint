8135d74ed6c16de24c962a97bbae450446cd610e
Frederick Sandalo <uscbsitric@gmail.com>
2017-01-19 15:35:58 +0000

    Firing an Event

--- app/Http/Controllers/EventsTestController.php
+++ app/Http/Controllers/EventsTestController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use Event;
+use App\User;
+use App\Events\TestEvent1;
 
 class EventsTestController extends Controller
 {
     public function eventtest()
     {
-    	exit('frederick debugging here');
+    	$user = User::where('id', '=', 1)->first();
+
+    	Event::fire(new TestEvent1($user));
+
+    	echo "<br>";
+    	exit('Testing Event Firing');
     }
 }
