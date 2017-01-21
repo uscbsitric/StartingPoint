6c47b04cf66c9acf5ff99f67122d3254d51b1256
Frederick Sandalo <uscbsitric@gmail.com>
2017-01-21 04:48:20 +0000

    Pushing TestJob1 into the Queue service of Laravel 5.2

--- app/Http/Controllers/QueueTestController.php
+++ app/Http/Controllers/QueueTestController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
+use App\Jobs\TestJob1;
 
 class QueueTestController extends Controller
 {
     public function jobEventTest()
     {
        $testVariable = 'test value';
+       $user = User::where('id', '=', 1)->first();
+       $this->dispatch(new TestJob1($user));
+
        exit('jobEventTest function of QueueTestController.');
     }
 }
